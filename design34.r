53e8e482c32f45284e82de3e8b349dd196b5230a
rPortelas <remy.portelas@gmail.com>
2016-11-24 10:00:36 -0800

    Add of design34 containing question 3 & 4

--- design34.r
+++ design34.r
@@ -1,29 +1,28 @@
 old.par <- par(mfrow = c(1, 2)) #this line and par(old.par) allow multiple plots for this problem
 							  #(http://www.dummies.com/programming/r/how-to-put-multiple-plots-on-a-single-page-in-r/)
-#Question 1
 
+#Question 3
 overtData = read.csv("Traffic_data_orig.csv")
 secretMessage = c(0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1,
 	 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0,
 	  0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 
 	  0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0,
 	   0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1)
-
 covertData = overtData[1:(length(secretMessage)+1),]
-zeroDelay = 0.25
-oneDelay = 0.75
 
-for (i in 2:(length(secretMessage)+1)){
-    #Insertion of the secret message using packet delays
-    if(secretMessage[i-1] == 0)
-        covertData[i,2] = zeroDelay
-    else
-        covertData[i,2] = oneDelay
-    #if(i != 1)
-    covertData[i,2] = covertData[i,2] + covertData[i-1,2]   
+#first we need to compute the inter-delays median, min and max of the overt distribution
+med = median(ipDelaysOvert)
+min = min(ipDelaysOvert)
+max = max(ipDelaysOvert)
+
+for (i in 2:(length(secretMessage) + 1)) {
+	#Insertion of the secret message using packet delays
+	if (secretMessage[i - 1] == 0)
+		covertData[i, 2] = sample(seq.int(min, med, 0.01), 1)
+	else
+		covertData[i, 2] = sample(seq.int(med, max, 0.01), 1)
+	covertData[i, 2] = covertData[i, 2] + covertData[i - 1, 2]
 }
-
-#Question 2
 
 #Instanciate vectors to store inter packet delays for PMF histograms
 ipDelaysCovert = numeric(nrow(covertData)-1)
@@ -53,6 +52,9 @@ names(probaCovert) = possibleInterDelays
 barplot(probaCovert, main = "PMF for X, inter-delays' RV of the covert channel", xlab = "k", ylab = "P(X=k)")
 names(probaOvert) = possibleInterDelays
 barplot(probaOvert, main = "PMF for X, inter-delays' RV of the overt channel", xlab = "k", ylab = "P(X=k)")
+
+
+
 
 
 
